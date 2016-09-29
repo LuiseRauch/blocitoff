c8ae48cc6429f3a61a3c96655147288837d82a89
Luise Rauch <luise.rauch@gmail.com>
2016-09-29 16:12:47 +0200

    User Story 6 - Create new tasks: completed

--- app/scripts/controllers/TaskCtrl.js
+++ app/scripts/controllers/TaskCtrl.js
@@ -3,14 +3,32 @@
       var ref = firebase.database().ref().child("tasks");
       // create a synchronized array
       $scope.tasks = $firebaseArray(ref);
+      $scope.sortType = 'priorityCode'; // set the default sort type
+      $scope.sortReverse = true;  // set the default sort order
+      $scope.sortClass = 'glyphicon glyphicon-sort';
+
+      var getPriorityCode = function(priorityText) {
+        var priorityCodes = {
+          'low': 1,
+          'med': 2,
+          'high': 3
+        };
+
+        return priorityCodes[priorityText];
+      };
+
       // add new items to the array
       // the task is automatically added to our Firebase database!
       $scope.addTask = function() {
         $scope.tasks.$add({
           text: $scope.newTaskText,
+          priority: $scope.newTaskPriority,
+          priorityCode: getPriorityCode($scope.newTaskPriority),
           created: firebase.database.ServerValue.TIMESTAMP,
           completed: false
         });
+        $scope.newTaskText = '';
+        $scope.newTaskPriority = '';
       };
 
       $scope.completeTask = function(task) {
